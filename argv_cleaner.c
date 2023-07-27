f4efb941066f23f3a147759b8895fc2b251eabeb
Albert Caballero Coll <alcaball@car11s1.42barcelona.com>
2023-07-27 15:02:03 +0200

    done some things

--- argv_cleaner.c
+++ argv_cleaner.c
@@ -6,20 +6,20 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 13:35:24 by alcaball          #+#    #+#             */
-/*   Updated: 2023/07/27 14:32:32 by alcaball         ###   ########.fr       */
+/*   Updated: 2023/07/27 15:01:47 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	**arg_parse(int argc, char **argv)
+int	*arg_parse(int argc, char **argv)
 {
 	int	i;
 	int	j;
 	int	*a;
 
 	i = 1;
-	while (argv[i] != NULL)
+	while (i <= argc)
 	{
 		j = 0;
 		while (argv[i][j] != 0)
@@ -30,13 +30,29 @@ int	**arg_parse(int argc, char **argv)
 		}
 		i++;
 	}
-	a = malloc(sizeof(int) * i);
-	j = 0;
+	a = malloc(sizeof(int) * (argc - 1));
 	i = 0;
-	while (argv[i + 1] != NULL)
+	while (i < argc)
 	{
 		a[i] = atoi(argv[i + 1]);
 		i++;
 	}
+	if (check_repeated(a, argc - 1) == 1)
+		return (-1);
 	return (a);
+}
+
+int	check_repeated(int *a, int count)
+{
+	int	val;
+	int	i;
+
+	while (i < count)
+	{
+		val = a[i];
+		i++;
+		if (a[i] == val)
+			return (1);
+	}
+	return (0);
 }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 12:48:58 by alcaball          #+#    #+#             */
-/*   Updated: 2023/07/27 14:32:29 by alcaball         ###   ########.fr       */
+/*   Updated: 2023/07/27 14:55:15 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@ int	main(int argc, char **argv)
 	int		*a;
 
 	if (argc < 2)
-		write (2, "Error\n", 6);
+		return (write (2, "Error\n", 6));
 	a = arg_parse(argc, argv);
+	if (a == -1)
+		return (write (2, "Error\n", 6));
 	return (0);
 }
