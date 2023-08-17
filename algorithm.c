7700db394945027ce7fc4caf9945a56214b95f63
Albert Caballero <albert@Alberts-MacBook-Pro.local>
2023-08-17 10:35:15 +0200

    started working on the algorithm

---
+++ algorithm.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albert <albert@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 00:57:07 by albert            #+#    #+#             */
+/*   Updated: 2023/08/17 10:34:54 by albert           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_num	*algorithm_main(t_num **a, t_num **b)
+{
+	//take first X, draw a median, if val>median push_b, else push_a
+	//outliers: https://articles.outlier.org/calculate-outlier-formula 
+}
