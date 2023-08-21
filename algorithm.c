fdd49731279b9cfa71639751d61b8f6577c7cee6
Albert Caballero Coll <alcaball@car14s2.42barcelona.com>
2023-08-21 14:17:53 +0200

    fasfd

--- algorithm.c
+++ algorithm.c
@@ -3,21 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   algorithm.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albert <albert@student.42.fr>              +#+  +:+       +#+        */
+/*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 00:57:07 by albert            #+#    #+#             */
-/*   Updated: 2023/08/17 19:48:58 by albert           ###   ########.fr       */
+/*   Updated: 2023/08/21 13:58:34 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	findcheap(t_num *stack)
+{
+	
+}
+
 t_num	*algorithm_main(t_num **a, t_num **b)
 {
-	int	size;
+	int	size_a;
 	int	maxval;
 	int	minval;
 
-	size = ft_lstsize (*a);
-	//https://medium.com/@jamierobertdawson/push-swap-the-least-amount-of-moves-with-two-stacks-d1e76a71789a
+	size_a = ft_lstsize (*a);
+	// https://medium.com/@ayogun/push-swap-c1f5d2d41e97 
+	push_b(*a, b);
+	push_b(*a, b);
+	findcheap(*a);
 }
