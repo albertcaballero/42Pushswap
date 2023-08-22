09d3bf2ab6d627af3c79250126d02f9463aab7bb
Albert Caballero Coll <alcaball@car12s2.42barcelona.com>
2023-08-22 12:43:00 +0200

    fkslad

--- algorithm.c
+++ algorithm.c
@@ -6,44 +6,54 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 00:57:07 by albert            #+#    #+#             */
-/*   Updated: 2023/08/21 17:35:00 by alcaball         ###   ########.fr       */
+/*   Updated: 2023/08/22 12:40:42 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	findtop(t_movs *movs, t_num *b, int aval)
+{
+	int	top;
+	int	j;
+	int	posmaxb;
+
+	top = -2147483648;
+	j = 0;
+	if (aval > max(b, VAL) || aval < min(b, VAL))
+	{
+		posmaxb = max(b, POS);
+		return (posmaxb);
+	}
+	else
+	{
+		while (b)
+		{
+			if (b->val > top && b->val < aval)
+				top = b->val;
+			j++;
+			b = b->next;
+		}
+		return (j);
+	}
+}
+
 int	findcheap(t_num *a, t_num *b)
 {
-	int		posmaxb;
 	int		i;
 	t_movs	movs;
 	t_movs	small;
-	int		top;
-	int		j;
+	int		index;
 
 	i = 0;
-	top = -2147483648;
-	small.tot = 800; //feo de collons, pero es para inicializarla en algo
+	small.tot = 800;
 	while (a)
 	{
-		if (a->val > max(b, VAL) || a->val < min(b, VAL))
-		{
-			posmaxb = max(b, POS);
-			if (posmaxb > (ft_lstsize (b) / 2))
-				movs.rrb = ft_lstsize (b) - posmaxb;
-			else
-				movs.rb = posmaxb;
-		}
+		index = findtop(&movs, b, a->val);
+		if (index > (ft_lstsize (b) / 2))
+			movs.rrb = ft_lstsize (b) - index;
 		else
-		{
-			while (b)
-			{
-				if (b->val > top && b->val < a->val)
-					top = b->val;
-				j++;
-				b = b->next; //AQUI TE QUEDASTE - NOW EITHER RB OR RRB UNTIL TOP->POS = 0
-			}
-		} //separarlo en funciones
+			movs.rb = index;
 		if (i > (ft_lstsize (a) / 2))
 			movs.rra = ft_lstsize (a) - i;
 		else
